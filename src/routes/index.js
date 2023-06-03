2db000a3148381202a1a1674a643fa97618d438e
Esneyder98 <esneydersa@hotmail.com>
2023-06-03 17:32:03 -0500

    modificando extructura de proyecto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const storeRouter = require('./storeRouter.js');
-const userRouter = require('./userRouter');
-const articleRouter = require('./articleRouter')
+const userRouter = require('./userRouter.js');
+const articleRouter = require('./articleRouter.js')
 function routerApi(app) {
   const router = express.Router();
   app.use("/api/v1",router); 
